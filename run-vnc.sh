6bfc18e7470222f2a3cf8d264ee935f5a6d94ae4
Kai <nankeen@users.noreply.github.com>
2021-03-12 14:35:59 +0000

    Update InfOS commit Add VNC run script

--- run-vnc.sh
+++ run-vnc.sh
@@ -7,4 +7,4 @@ ROOTFS=$INFOS_USER_DIR/bin/rootfs.tar
 ISO=$TOP/build/infos.iso
 QEMU=qemu-system-x86_64
 
-$QEMU -cdrom $ISO -m 6G -debugcon stdio -hda $ROOTFS -s
+$QEMU -cdrom $ISO -m 6G -debugcon stdio -hda $ROOTFS -display vnc=localhost:1234 -S -s
